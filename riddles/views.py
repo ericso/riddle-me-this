028ddc110dafdd0307f1f1872470f65d0d14e982
Eric So <me@eric.so>
2015-02-22 20:02:58 -0800

    Refactor home page view to use a template

--- riddles/views.py
+++ riddles/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-  return HttpResponse('<html><title>Riddles</title></html>')
+  return render(request, 'home.html')
