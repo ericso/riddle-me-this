650468168ee187a82ea2662b79290ae5f8d3b2d2
Eric So <me@eric.so>
2015-02-22 17:41:52 -0800

    Passing UTs, failing FTs

--- riddles/tests.py
+++ riddles/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 
 from riddles.views import home_page
 
@@ -10,3 +10,11 @@ class HomePageTest(TestCase):
   def test_root_url_resolves_to_the_home_page(self):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
+
+  def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+
+    self.assertTrue(response.content.startswith(b'<html>'))
+    self.assertIn(b'<title>Riddles</title>', response.content)
+    self.assertTrue(response.content.endswith(b'</html>'))
